94652baaf61b0709241a28e69e4d26bbf4073f92
David Hurley <davehur@microsoft.com>
2017-07-24 13:51:30 -0400

    add unit tests

---
+++ src/FactOrFiction/FactOrFictionTest/ParserTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace FactOrFictionTest
+{
+
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using FactOrFictionWeb.Parser;
+
+    [TestClass]
+    public class Parser
+    {
+        [TestMethod]
+        public void ShittyParserTest()
+        {
+            var input = "fuck this fucking shit";
+            var result = ShittyParser.Parse(input, '.');
+            result.Should().Equal(input);
+        }
+    }
+}
