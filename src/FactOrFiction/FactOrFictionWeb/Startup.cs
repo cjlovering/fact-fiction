35b6d45a01a07af5d5db22fe1c6f284c4154aaff
David Hurley <davehur@microsoft.com>
2017-07-23 22:00:30 -0400

    starter code

---
+++ src/FactOrFiction/FactOrFictionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FactOrFictionWeb.Startup))]
+namespace FactOrFictionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
