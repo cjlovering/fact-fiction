d9cbe3983d9eb9f82f6a8d2c4c42c28d30429034
Aleksandar Milicevic <almili@microsoft.com>
2017-07-24 14:27:41 -0700

    add UrlSuggestions project

---
+++ src/FactOrFiction/FactOrFictionUrlSuggestions/BingV7Finder.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactOrFictionUrlSuggestions
+{
+    public sealed class BingV7Finder : BingFinder
+    {
+        protected override string BingUrl { get; } = "https://api.cognitive.microsoft.com/bing/v7.0/search";
+        protected override string CognitiveServicesSubscriptionKey { get; } = "d0ae93fad8c249fda81005fb56d79b3d";
+
+        protected override IReadOnlyList<Uri> ParseJson(JObject json)
+        {
+            return json["webPages"]["value"]
+                .Select(obj => new Uri(obj["url"].ToString()))
+                .ToList();
+        }
+    }
+}
---
+++ src/FactOrFiction/FactOrFictionUrlSuggestions/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactOrFictionUrlSuggestions
+{
+    public interface IFinder
+    {
+        /// <summary>
+        /// For a given string query, returns a list of suggested URLs.
+        /// </summary>
+        Task<IReadOnlyList<Uri>> FindSuggestions(string query);
+    }
+    
+
+
+    public interface URLClassifier
+    {
+        
+        /// <summary>
+        /// For a given string query, representing a news outlet (eg. nytimes), returns a classification
+        /// </summary>
+        Task<String> ClassifyOutletDescription(string description);
+    }
+}
