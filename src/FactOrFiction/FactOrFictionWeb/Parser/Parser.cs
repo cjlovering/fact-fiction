5d1afdcc88398659235e683a48881ca0f97943b9
Tamara Balderas <t-tabald@microsoft.com>
2017-07-24 16:17:34 -0400

    Bugfix parser

--- src/FactOrFiction/FactOrFictionWeb/Parser/Parser.cs
+++ src/FactOrFiction/FactOrFictionWeb/Parser/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -21,19 +22,21 @@ namespace FactOrFictionWeb.Parser
 
                 if (!Char.IsLetter(curr) || !Char.IsNumber(curr))
                 {
-                    tupleList.Add(new Tuple<int, int>(start, index));
-                    start = index + 1;
-
-                    if (curr == ',' && Char.IsNumber(prev) && Char.IsNumber(next)) // if curr is a comma separating number
+                    if ((curr == ',' || curr == '.') && Char.IsNumber(prev) && Char.IsNumber(next)) // if curr is a comma separating number
                     {
                         continue;
                     }
+
+                    tupleList.Add(new Tuple<int, int>(start, index));
+                    start = index + 1;
                 }
                 index++;
             }
 
             return tupleList.Select(x => input.Substring(x.Item1, x.Item2 - x.Item1)).ToArray();
         }
+        
+
         public static string[] puctuationParse(string input, char delimiter)
         {
             var start = 0;
@@ -43,14 +46,27 @@ namespace FactOrFictionWeb.Parser
             while (index < input.Length)
             {
                 char ch = input[index];
-                if (ch == '.' || ch == '-')
+                char prev = input[Math.Max(0, index - 1)];
+                char next = input[Math.Min(input.Length - 1, index + 1)];
+                char nextnext = input[Math.Min(input.Length - 1, index + 2)];
+
+                if (ch == '.' || ch == 8212 || ch == 63 || ch == 33 || ch == 8211) // . 
                 {
-                    tupleList.Add(new Tuple<int, int>(start, index));
+                    if (ch == '.' 
+                        && ((Char.IsNumber(prev) && Char.IsNumber(next)) 
+                                || (Char.IsUpper(prev) && (Char.IsUpper(next) || next == ',' || Char.IsLower(nextnext))))) // if curr is a period separating number
+                    {
+                        index++;
+                        continue;
+                    }
                     start = index + 1;
                 }
                 index++;
             }
-            return tupleList.Select(x => input.Substring(x.Item1, x.Item2 - x.Item1)).ToArray();
+            string[] tokens = tupleList.Select(x => input.Substring(x.Item1, x.Item2 - x.Item1))
+                .Select(x => x)
+                .ToArray();
+            return tokens;
         }
 
         public static string[] quoteParse(string input, char delimiter)
