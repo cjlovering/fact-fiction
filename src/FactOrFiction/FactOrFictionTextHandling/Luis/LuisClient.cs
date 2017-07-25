d2bbcd0083f207a03280812377c1a89aadad4d3c
Bryan Jeffrey <brjeffre@microsoft.com>
2017-07-25 16:58:56 +0000

    Merged PR 12: Merge AddLuisClient to master

---
+++ src/FactOrFiction/FactOrFictionTextHandling/Luis/LuisClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FactOrFictionTextHandling.Luis
+{
+    public class LuisClient : ILuisClient
+    {
+        public WebClient Client { get; private set; }
+        public string BaseUri { get; set; }
+
+        public LuisClient(LuisWebClient client, string baseUri)
+        {
+            Client = client;
+            BaseUri = baseUri;
+        }
+
+        public string Query(string sentenceFragment)
+        {
+            var uri = new Uri(this.BaseUri + HttpUtility.UrlEncode(sentenceFragment));
+            return Client.DownloadString(uri);
+        }
+    }
+}
---
+++ src/FactOrFiction/FactOrFictionTextHandling/Luis/LuisClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactOrFictionTextHandling.Luis
+{
+    public class LuisClientFactory : ILuisClientFactory
+    {
+        public LuisWebClient Client { get; set; }
+        public string BaseUri { get; set; }
+
+        public LuisClientFactory(LuisWebClient client, string baseUri)
+        {
+            Client = client;
+            BaseUri = baseUri;
+        }
+
+        public ILuisClient Create()
+        {
+            return new LuisClient(this.Client, this.BaseUri);
+        }
+    }
+}
