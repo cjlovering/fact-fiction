f81620ca137155e819b1630a5e3f38b8f6072588
Cuong Dinh Tri Nguyen (Worcester Polytechnic Inst.) <v-cun@microsoft.com>
2017-11-28 19:47:49 +0000

    Merged PR 78: Setup for deployment
    + split webpack into prod and dev
    + changed webpack command
    + moved style and script link out of Dev env

---
+++ src/FactOrFictionCore/FactOrFictionFrontend/webpack.prod.js
@@ -0,0 +1,13 @@
+const merge = require('webpack-merge');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new MinifyPlugin()
+    ]
+});
