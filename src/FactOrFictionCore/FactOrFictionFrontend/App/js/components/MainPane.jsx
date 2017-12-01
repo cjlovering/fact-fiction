f72cdcd1487a570aac94f6673a26305eb633905e
Huyen Nguyen (Worcester Polytechnic Inst.) <v-hunguy@microsoft.com>
2017-12-01 23:47:02 +0000

    Merged PR 98: Added tooltip
    Added tooltip

--- src/FactOrFictionCore/FactOrFictionFrontend/App/js/components/MainPane.jsx
+++ src/FactOrFictionCore/FactOrFictionFrontend/App/js/components/MainPane.jsx
@@ -4,7 +4,6 @@ import {
     Spinner,
     SpinnerSize
 } from 'office-ui-fabric-react/lib/Spinner';
-
 import InputPane from './InputPane';
 import ListView from './ListView';
 import Button from './Button';
@@ -13,6 +12,8 @@ import ResultPane from './ResultPane';
 
 import { VIEW_INPUT } from '../constants/viewTypes';
 import _ from '../../stylesheets/components/_MainPane.scss';
+
+import ReactTooltip from 'react-tooltip'
 
 export default class MainPane extends React.Component {
     static propTypes = {
@@ -65,6 +66,12 @@ export default class MainPane extends React.Component {
         const isInput = view === VIEW_INPUT;
         const leftPaneTitle = isInput ? "Input" : "Results";
         const middlePaneTitle= isInput ? "Feed" : "Objective Statements";
+        const toolTipTextMainPane = isInput ? 
+            "Paste your news article into this textbox<br/>to classify objective and subjective sentences." :
+            "The highlighted sentences are objective.<br/>The bar displays the percentage of objective<br/>sentences out of all sentences submitted.";
+        const tooltipTextObjectiveStatements = isInput ?
+            "This is your objective feeds.<br/>You can vote on them now!" : 
+            "These are objective statements from your submission.";
         const leftPane = isInput ? (
             <InputPane
                 fetchTextEntry={fetchTextEntry}
@@ -138,16 +145,20 @@ export default class MainPane extends React.Component {
         />
         return (
             <div className="row"> 
+                <ReactTooltip place="right" type="dark" effect="float" multiline={true}/>
                 <div className="col-sm-12 col-md-12 col-lg-4 col-xl-5">
                     <span className="box-title ms-font-xxl ms-fontColor-themePrimary">{leftPaneTitle}</span>
+                    <span className="help-icon fa fa-question-circle-o " aria-hidden="true" data-tip={toolTipTextMainPane}></span>
                     {leftPane}
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3-5">
                     <span className="list-title ms-font-xxl ms-fontColor-themePrimary">{middlePaneTitle}</span>
+                    <span className="help-icon fa fa-question-circle-o " aria-hidden="true" data-tip={tooltipTextObjectiveStatements}></span>
                     {middlePane}
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3-5">
                     <span className="list-title ms-font-xxl ms-fontColor-themePrimary">Similar Sentences</span>
+                    <span className="help-icon fa fa-question-circle-o " aria-hidden="true" data-tip="These are sentences related to<br/>your selected objective sentence."></span>
                     {rightPane}
                 </div>
             </div>
