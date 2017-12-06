905ca6fc4e3cc66c0716b17b1679f27a841c56c0
Cuong Dinh Tri Nguyen (Worcester Polytechnic Inst.) <v-cun@microsoft.com>
2017-12-06 15:33:41 +0000

    Merged PR 108: Welcome page
    Welcome page

--- src/FactOrFictionCore/FactOrFictionFrontend/Controllers/HomeController.cs
+++ src/FactOrFictionCore/FactOrFictionFrontend/Controllers/HomeController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace FactOrFictionFrontend.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return RedirectToAction("Create", "TextEntries");
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Create", "TextEntries");
+            }
+            return View();
         }
     }
 }
